8d88ec272af42b51a9d20768ad71f57ac6f57a5f
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2021-09-01 19:21:42 -0700

    Added assertion messages for List

--- structs/list/main.py
+++ structs/list/main.py
@@ -1,5 +1,6 @@
 from list import List
 
+print("STARTING TEST")
 my_list = List()
 my_list.append(1)
 my_list.append(2)
@@ -7,18 +8,22 @@ my_list.append(3)
 
 # Test expected value
 assert my_list.__repr__() == "1 -> 2 -> 3 -> None"
+print("PASS List.__repr__()")
 
 # Test find() 
 assert my_list.find(9) == False
 assert my_list.find(2) == True
+print("PASS List.find()")
 
 # Test delete()
 my_list.delete(1)
 assert my_list.find(1) == False
+print("PASS List.delete()")
 
 # Test append()
 assert my_list.find(4) == False
 my_list.append(4)
 assert my_list.find(4) == True
+print("PASS List.append()")
 
-print("TEST PASS")
+print("SUCCESS")
