8e675d9523be0e3606723caa5ac38d8645029897
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2021-09-02 16:02:18 -0700

    Added List.prepend, List.find_nth, List.find_middle, List.find_nth_last, List.replace, List.suma

--- structs/list/list.py
+++ structs/list/list.py
@@ -20,7 +20,12 @@ class List:
         while current.next is not None:
             current = current.next
         current.next = node
-    
+        
+    def prepend(self, value):
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+
     def find(self, search_value):
         if self.head is None:
             return False
@@ -32,6 +37,40 @@ class List:
             current = current.next
         return False
     
+    def find_nth(self, n):
+        if self.head is None:
+            return None
+        current = self.head
+        for i in range(1, n):
+            current = current.next
+        return current.value
+    
+    def find_nth_last(self, n):
+        if self.head is None:
+            return None
+        
+        length = 0
+        current = self.head
+        while current is not None:
+            length += 1
+            current = current.next
+        
+        current = self.head
+        for i in range(0, length - n):
+            current = current.next
+        return current.value
+
+    def find_middle(self):
+        if self.head is None:
+            return None
+        
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow.value
+
     def delete(self, value):
         if self.head is None:
             return False
@@ -53,5 +92,25 @@ class List:
     def is_empty(self):
         return self.head is None
     
+    def replace(self, n, m):
+        if self.head is None:
+            return False
+        
+        current = self.head
+        while current is not None:
+            if current.value == n:
+                current.value = m
+                return True
+            current = current.next
+        return False
+
+    def suma(self):
+        suma = 0
+        current = self.head
+        while current is not None:
+            suma += current.value
+            current = current.next
+        return suma
+
     def __repr__(self):
         return f"{self.head}"
--- structs/list/main.py
+++ structs/list/main.py
@@ -26,6 +26,34 @@ my_list.append(4)
 assert my_list.find(4) == True
 print("PASS List.append()")
 
+# Test prepend()
+my_list.prepend(6) 
+my_list.prepend(5) 
+assert my_list.__repr__() == "5 -> 6 -> 2 -> 3 -> 4 -> None"
+print("PASS List.prepend()")
+
+# Test find_nth()
+assert my_list.find_nth(1) == 5
+print("PASS List.find_nth()")
+
+# Test find_nth_last()
+assert my_list.find_nth_last(2) == 3
+print("PASS List.find_nth_last()")
+
+# Test find_middle()
+assert my_list.find_middle() == 2
+print("PASS List.find_middle()")
+
+# Test replace()
+my_list.replace(6,9) 
+assert my_list.__repr__() == "5 -> 9 -> 2 -> 3 -> 4 -> None"
+print("PASS List.replace()")
+
+# Test suma()
+my_list.suma() 
+assert my_list.suma() == 23
+print("PASS List.suma()")
+
 # Test is_empty()
 assert my_list.is_empty() == False
 my_list2 = List()
