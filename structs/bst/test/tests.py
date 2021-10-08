a4f5ba6053a9652c813ef01234f9257d306a11eb
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2021-10-08 15:10:36 -0700

    Added unittest testing to bst.

--- structs/bst/test/tests.py
+++ structs/bst/test/tests.py
@@ -48,61 +48,287 @@ class BstTest(unittest.TestCase):
         self.assertFalse(find(root, 13))
 
     def test_delete(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 2)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 9)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 8)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+        delete(root, 10)
+        delete(root, 100)
+
+        self.assertFalse(find(root, 10))
+        self.assertFalse(find(root, 100))
 
     def test_inorder(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        lista_inorder = []
+        inorder(root, lista_inorder)
+        self.assertEqual(lista_inorder, [1, 3, 4, 5, 6, 7, 10, 100, 200])
 
     def test_preorder(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+        
+        lista_preorder = []
+        preorder(root, lista_preorder)
+        self.assertEqual(lista_preorder, [4, 1, 3, 6, 5, 7, 10, 100, 200])
 
     def test_postorder(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+        
+        lista_postorder = []
+        postorder(root, lista_postorder)
+        self.assertEqual(lista_postorder, [3, 1, 5, 200, 100, 10, 7, 6, 4])
     
+    # Level order using queue data structure
     def test_bfs(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
 
+        self.assertEqual(bfs(root), [4, 1, 6, 3, 5, 7, 10, 100, 200])
+        
     def test_level_order(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(level_order(root), [[4], [1, 6], [3, 5, 7], [10], [100], [200]])
 
     def test_dfs(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(dfs(root), [4, 1, 3, 6, 5, 7, 10, 100, 200])
 
     def test_mirror(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        lista_inorder = []
+        inorder(root, lista_inorder)
+        lista_inorder.reverse() 
+
+        mirror(root)
+        lista_inorder_mirror = []
+        inorder(root, lista_inorder_mirror)
+
+        self.assertEqual(lista_inorder, lista_inorder_mirror)
 
     def test_is_same(self):
-        pass
+        root1 = Node(4)
+        insert(root1, 1)
+        insert(root1, 2)
+        insert(root1, 3)
+        insert(root1, 6)
+
+        root2 = Node(4)
+        insert(root2, 1)
+        insert(root2, 2)
+        insert(root2, 3)
+        insert(root2, 6)
+
+        root3 = Node(4)
+        insert(root3, 1)
+        insert(root3, 2)
+        insert(root3, 9) 
+        insert(root3, 6)
+
+        self.assertEqual(is_same(root1, root2), True)
+        self.assertTrue(is_same(root1, root2))
+        self.assertEqual(is_same(root1, root3), False)
+        self.assertFalse(is_same(root1, root3))
+        self.assertEqual(is_same(root2, root3), False)
+        self.assertFalse(is_same(root2, root3))
 
     def test_size(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(size(root), 9)
 
     def test_size_2(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(size_2(root), 9)
 
     def test_size_3(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(size_3(root), 9)
     
     def test_size_4(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(size_4(root), 2)
 
     def test_find_tree_height(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(find_tree_height(root), 6)
 
     def test_min_value(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(min_value(root), 1)
 
     def test_max_value(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(max_value(root), 200)
 
     def test_find_max_path(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(find_max_path(root), 327)
 
     def test_find_min_path(self):
-        pass
+        root = Node(4)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 6)
+        insert(root, 7)
+        insert(root, 5)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        self.assertEqual(find_min_path(root), 5)
 
     def test_root_to_leaf_path(self):
-        pass
+        root = Node(5)
+        insert(root, 1)
+        insert(root, 3)
+        insert(root, 7)
+        insert(root, 6)
+        insert(root, 10)
+        insert(root, 100)
+        insert(root, 200)
+
+        current_path = []
+        all_paths = []
+        expected = root_to_leaf_path(root, current_path, all_paths)
+        self.assertEqual(expected, [[5, 1, 3], [5, 7, 6], [5, 7, 10, 100, 200]])
 
     def test_root_to_leaf_path_sum(self):
         pass
