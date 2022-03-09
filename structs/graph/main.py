5f42bf83b094f0b6b2788bbbe7ba1cf789bd37fb
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2022-03-08 16:44:20 -0800

    DFS graph traversal.

--- structs/graph/main.py
+++ structs/graph/main.py
@@ -1,5 +1,6 @@
 from Graph import Graph
 from Queue import MyQueue
+from Stack import MyStack
 
 g = Graph(4)
 g.add_edge(0, 2)
@@ -52,6 +53,38 @@ def bfs_traversal(g, source):
             result += result_new
     return result
 
+def dfs_traversal_helper(g, source, visited):
+    result = ""
+    stack = MyStack()
+    stack.push(source)
+    visited[source] = True
+    while not stack.is_empty():
+        current_node = stack.pop()
+        result += str(current_node)
+        temp = g.array[current_node].head
+        while temp is not None:
+            if not visited[temp.value]:
+                stack.push(temp.value)
+                visited[temp.value] = True
+            temp = temp.next
+    return result, visited  
+
+def dfs_traversal(g, source):
+    result = ""
+    num_of_vertices = g.vertices
+    if num_of_vertices is 0:
+        return result
+    visited = []
+    for i in range(num_of_vertices):
+        visited.append(False)
+    result, visited = dfs_traversal_helper(g, source, visited)
+    for i in range(num_of_vertices):
+        if not visited[i]:
+            result_new, visited = dfs_traversal_helper(g, i, visited)
+            result += result_new
+    return result
+
+
 if __name__ == "__main__" :
     g = Graph(4)
     num_of_vertices = g.vertices
@@ -65,3 +98,17 @@ if __name__ == "__main__" :
         g.add_edge(1, 3)
         g.add_edge(2, 3)
         print(bfs_traversal(g, 0))
+
+    g2 = Graph(7)
+    num_of_vertices = g2.vertices
+    if num_of_vertices == 0:
+        print("Graph is empty")
+    elif num_of_vertices < 0:
+        print("Graph cannot have negative vertices")
+    else:
+        g2.add_edge(1, 2)
+        g2.add_edge(1, 3)
+        g2.add_edge(2, 4)
+        g2.add_edge(2, 5)
+        g2.add_edge(3, 6)
+        print(dfs_traversal(g2, 1))
