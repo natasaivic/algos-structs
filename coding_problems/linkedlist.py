4eff4b496eef3ad68fd28470ef97cb45be5f9aac
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2022-03-17 21:19:59 +0100

    Supplementary print function and insert at head. .

--- coding_problems/linkedlist.py
+++ coding_problems/linkedlist.py
@@ -24,9 +24,33 @@ class LinkedList:
             return True
         else:
             return False
+            
+    def insert_at_head(self, data):
+        temp_node = Node(data)
+        temp_node.next_element = self.head_node
+        self.head_node = temp_node  # Make the head point to the new node
+        return self.head_node 
 
+    # Supplementary print function
+    def print_list(self):
+        if(self.is_empty()):
+            print("List is Empty")
+            return False
+        temp = self.head_node
+        while temp.next_element is not None:
+            print(temp.data, end=" -> ")
+            temp = temp.next_element
+        print(temp.data, "-> None")
+        return True
 
 lst = LinkedList()  # Linked List created
 print(lst.is_empty())  # Returns true
+lst.print_list()
+
+print("Inserting values in list")
+for i in range(1, 10):
+    lst.insert_at_head(i)
+lst.print_list()
+
 
 
