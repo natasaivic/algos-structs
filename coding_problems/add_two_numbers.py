66ccaa9dd3fb1b70321e4e71d0732549ca47acfa
Natasa Ivic <natasa.popovic.ivic@gmail.com>
2022-03-30 23:15:59 +0200

    Different approach.

--- coding_problems/add_two_numbers.py
+++ coding_problems/add_two_numbers.py
@@ -27,3 +27,23 @@ class Solution:
             if l2:
                 l2 = l2.next
         return ans.next
+
+class Solution2:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        current = dummy
+
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            current.next = ListNode(val)
+
+            # update pointers
+            current = current.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
